174fca331de234561c29128caf4dc7d07a716e95
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-13 01:15:29 +0200

    V0.5 - Añadiendo JS

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+
+//Constante declarando pantalla y donde identificarla
+const pantalla = document.getElementById("pantalla");
+
+//Intento de añadir valor de tecla a pantalla
+function mostrarTecla (valor){
+    pantalla.getElementById +=valor;
+}

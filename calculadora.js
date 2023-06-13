d2db6877525614bd5a70f3ae3ba5e99e866ad13b
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-13 08:19:13 +0200

    V0.6 - Calculadora 'funcional'

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,26 @@
 
+let valores = [];
 //Constante declarando pantalla y donde identificarla
 const pantalla = document.getElementById("pantalla");
 
 //Intento de añadir valor de tecla a pantalla
 function mostrarTecla (valor){
-    pantalla.getElementById +=valor;
+    pantalla.value += valor;
+    valores.push (valor);
+}
+
+//Modifica el valor en pantalla a caracter vacío
+function limpiarCaja(){
+    pantalla.value = '';
+}
+
+//Hace la operación y muestra el resultado por pantalla
+function resultado(){
+    let resultado;
+    if (valores[1]="+") {
+        //Necesario para convertir valores a numeros y no concatenar los caracteres
+        resultado = parseFloat(valores[0])+parseFloat(valores[2]);
+        limpiarCaja();
+        pantalla.value = resultado;
+    }
 }

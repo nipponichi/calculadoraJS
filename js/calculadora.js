00066f60c4288a809a280d17dfbf4f51230c4e7f
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-15 08:56:20 +0200

    V.08 - Añadida potencia, control de error y fix errores

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,5 +1,5 @@
 
-let valoresEspacio = '';
+let valoresEspacio = [];
 let valores = [];
 //Constante declarando pantalla y donde identificarla
 const pantalla = document.getElementById("pantalla");
@@ -35,34 +35,61 @@ function mostrarSigno(valor) {
 function verificarValores() {
     if (valores.length == 0) {
       alert("Inserta valores");
+      return true;
+    } else {
+        return false;
     }
+}
+
+//Mensaje alert si no introducimos valores y clickamos '='
+function verificarResultado() {
+    if (valores[0] >= 999999999 || valores[2] >= 999999999) {
+      alert("Se ha excedido el poder de la calculadora, hazte premium");
+      return true;
+    } else {
+        return false;
+    }
+}
+
+//Calcula la potencia de un número
+function potencia(){
+    valores.push(parseFloat(pantalla.value.split(' ')));
+    resultado = parseFloat(valores[0]) * parseFloat(valores[0]);
+    limpiarValores(resultado);
+    valores.splice(0,3);
 }
   
 //Hace la operación y muestra el resultado por pantalla
 function calcularResultado(){
-    verificarValores();
     let resultado;
     valoresEspacio = pantalla.value.split(' ');
-    valores.push(valoresEspacio[2])
+    valores.push(valoresEspacio[2]);
+
+    //Verificamos si hay valores, o los valores son demasiado grandes
+    if (verificarValores() || verificarResultado()){
+        limpiarValores();
+        limpiarPantalla();
+        return;
+    }
     valoresEspacio.splice(0,3);
 
     //Según el valor encontrado en la posición 1 del array, realiza una operación.
     switch(valores[1]){
         case '+':
-            resultado = parseFloat(valores[0]) + parseFloat(valores[2])
+            resultado = parseFloat(valores[0]) + parseFloat(valores[2]);
             limpiarValores(resultado);
             break;
         case '-':
-            resultado = parseFloat(valores[0]) - parseFloat(valores[2])
+            resultado = parseFloat(valores[0]) - parseFloat(valores[2]);
             limpiarValores(resultado);
             break;
         case '/':
-            resultado = parseFloat(valores[0]) / parseFloat(valores[2])
+            resultado = parseFloat(valores[0]) / parseFloat(valores[2]);
             limpiarValores(resultado);
             break;
         case '*':
-            resultado = parseFloat(valores[0]) * parseFloat(valores[2])
+            resultado = parseFloat(valores[0]) * parseFloat(valores[2]);
             limpiarValores(resultado);
             break;
-    }   
+    }  
 }
